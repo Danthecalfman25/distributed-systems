ce055cf427a463777c598fe3d2428b762716c3a1
Danthecalfman <spikeeboy@outlook.com>
2025-10-14 17:40:44 -0700

    starting sending coordinates

--- pubsub/driver_pub.py
+++ pubsub/driver_pub.py
@@ -3,12 +3,21 @@ import time
 import json
 import random
 
+
+def get_location():
+    longitude = random.randint(0,1000)
+    lattitute = random.randint(0,1000)
+    return f"{longitude}, {lattitute}"
+
 def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
     channel = connection.channel()
     channel.exchange_declare(exchange = 'delivery', exchange_type='topic')
     channel.queue_declare(queue='customer_updates')
-    channel.basic_publish(exchange='delivery', routing_key='driver.location.1', body = 'location:')
+    for i in range(5):
+        channel.basic_publish(exchange='delivery', routing_key='driver.location.1', body = get_location())
+        print("Sent Location")
+        time.sleep(5)
     print("Sent Location")
     connection.close()
 
